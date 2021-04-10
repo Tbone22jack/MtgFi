6ab1ff397df3ebef24eb0cb37f112b140ba0f15b
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-04-10 02:52:14 -0500

    cut down download speed from 5 days to 1 day

--- Scraper.py
+++ Scraper.py
@@ -9,7 +9,9 @@ from time import sleep
 from tqdm import tqdm
 import datetime
 
-
+#new-iteration started at 1:55am
+#started again around 2:37
+#TODO: make a list of cards that give it a 404
 
 
 def file_generator(filename):
@@ -35,25 +37,28 @@ requests.get(strUrl)
 progress = tqdm(desc=f"Counting Cards", total=21744)
 
 def url_maker(set_name, card_name):
-    set_name = set_name.replace(" ","+").replace("'",'').replace(':','').replace('.','')
-    card_name = card_name.replace(' // ','+').replace(', ', '+').replace(" ","+").replace("'",'')
+    set_name = set_name.replace(' / ','+').replace(' // ','+').replace(" ","+").replace("'",'').replace(':','').replace('.','').replace('-','+')
+    card_name = card_name.replace(' / ','+').replace(' // ','+').replace(', ', '+').replace(" ","+").replace("'",'').replace('-','+')
     return set_name + '/' + card_name
 
 
 for index, row in tqdm(cards.iterrows()):
     card_url = url_maker(row.loc['setName'], row.loc['name'])
-    card_info_filepath = os.path.join(dir_path, "Card_Text", card_url.replace('/','_') + '.txt')
+    #card_info_filepath = os.path.join(dir_path, "Card_Text", card_url.replace('/','_') + '.txt')
     price_info_filepath = os.path.join(dir_path, "Price_Info", card_url.replace('/','_') + '.txt')
-    if os.path.exists(card_info_filepath) and os.path.exists(price_info_filepath):
+    if os.path.exists(price_info_filepath):
         progress.update(1)
         continue
 
+    if index <= 313:
+        progress.update(1)
+        continue
 
     print(card_url)
 
 
     # wait for a random amount of time
-    sleep_time  = randint(5,10)
+    sleep_time  = randint(3,5)
     sleep(sleep_time)
 
     
@@ -74,16 +79,16 @@ for index, row in tqdm(cards.iterrows()):
     strDates = '\n'.join(lstDates)
 
     # get text about card
-    parsed_html = BeautifulSoup(html, 'html.parser')
-    strOracle = str(parsed_html.body.find('div', attrs={'id':'oracle-text'}))
-    # clean text
-    strOracle = re.sub('<br\/\s*?>', ' ', strOracle)
-    strOracle = re.sub(r'<(.|\n)*?>', "", strOracle).replace('\n', ' ')
-    strOracle = re.sub(r' +', ' ', strOracle).strip()
+    # parsed_html = BeautifulSoup(html, 'html.parser')
+    # strOracle = str(parsed_html.body.find('div', attrs={'id':'oracle-text'}))
+    # # clean text
+    # strOracle = re.sub('<br\/\s*?>', ' ', strOracle)
+    # strOracle = re.sub(r'<(.|\n)*?>', "", strOracle).replace('\n', ' ')
+    # strOracle = re.sub(r' +', ' ', strOracle).strip()
 
     # write info in appropriate place
-    with open(card_info_filepath, 'w') as file_handler:
-        file_handler.write(strOracle)
+    #with open(card_info_filepath, 'w') as file_handler:
+    #    file_handler.write(strOracle)
 
     with open(price_info_filepath, 'w') as file_handler:
         file_handler.write(strDates)
