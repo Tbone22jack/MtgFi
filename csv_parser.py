688596d4cfc4ede7f54a9a44aed9f02aea642455
Jv Kyle Eclarin <mathemusician@users.noreply.github.com>
2021-04-09 23:58:31 -0500

    update csv parser to find the earliest release date for each card; also it takes away all the land cards

--- csv_parser.py
+++ csv_parser.py
@@ -1,20 +1,61 @@
 import pandas as pd
-import numpy
+import numpy as np
 import time
+import os
+from tqdm import tqdm
 
-#This program extracts out the name and set code for each card and matches that code with a set name
-#and exports a csv with the card name, set code, and set name for scraping later
-cards_col = ["name","code","uuid"]
-sets_col = ["code","setName"]
-cards = pd.read_csv("C:/Users/thoma/Downloads/AllPrintingsCSVFiles/cards.csv", usecols = ["setCode","name","uuid"])
-cards.columns = cards_col
-print(cards)
-sets = pd.read_csv("C:/Users/thoma/Downloads/AllPrintingsCSVFiles/sets.csv", usecols = ["code","name"])
-sets.columns = sets_col
-combine = pd.merge(cards, sets,on ="code")
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-#This portion is optional as this just reduces the load
-combine = combine.drop_duplicates(subset = ['name'])
+print('This program extracts out the name and set code for each card and matches that code with a set name')
+print('and exports a csv with the card name, set code, and set name for scraping later')
 
-print(combine)
-combine.to_csv("C:/Users/thoma/Desktop/Magic Project/data/cards0.csv")
+cards = pd.read_csv(os.path.join(dir_path,"MTGJSON","AllPrintingsCSVFiles","cards.csv"),
+                    usecols = ["name","setCode", "originalType"])
+cards.columns = ["name","type", "setCode"]
+
+sets = pd.read_csv(os.path.join(dir_path,"MTGJSON","AllPrintingsCSVFiles","sets.csv"),
+                   usecols = ["name","code", "releaseDate"])
+sets.columns = ["setCode","setName","releaseDate"]
+
+# add cards together
+combine = cards.merge(sets, on='setCode')
+combine = combine.drop(['setCode'], axis=1)
+
+# drop lands
+boolean_land = combine["type"] == "Land"
+combine = combine.loc[~boolean_land]
+
+# drop duplicates
+card_names = combine['name'].drop_duplicates().tolist()
+
+# Drop duplicates of cards with the same name but keep the earliest release date
+releaseDates = []
+setNames = []
+
+for index in tqdm(range(len(card_names))):
+    boolean_name_match = combine['name'] == card_names[index]
+    # if name is unique, just add it to lists
+    if boolean_name_match.sum() == 1:
+        releaseDates.append(name_match['releaseDate'].iloc[0])
+        setNames.append(name_match['setName'].iloc[0])
+        continue
+    
+    # otherwise, iterate through dates and pick the smallest one
+    name_match = combine.loc[boolean_name_match]
+    lowest_date = name_match['releaseDate'].iloc[0]
+    index_lowest_date = 0
+    for j in range(1,len(name_match)):
+        temp_date = name_match['releaseDate'].iloc[j]
+        if temp_date < lowest_date:
+            lowest_date = name_match['releaseDate'].iloc[j]
+            index_lowest_date = j
+    
+    releaseDates.append(lowest_date)
+    setNames.append(name_match['setName'].iloc[j])
+
+filtered_cards = pd.DataFrame({'name':card_names,
+                               'setName':setNames,
+                               'releaseDates':releaseDates})
+
+print(filtered_cards.head(5))
+filtered_cards.to_csv(os.path.join(dir_path,"cards.csv"))
